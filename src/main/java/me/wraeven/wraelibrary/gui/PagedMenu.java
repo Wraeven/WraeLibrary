392e5588fb830fb851d0074980797841c490d4cd
Wraeven <wraevenly@gmail.com>
2024-06-30 21:42:51 +0100

    Update PagedMenu constructor to handle maxItemsPerPage
    The constructor for PagedMenu now takes an argument of maxItemsPerPage and validates this number. The function previously used for validating has been made private, renamed to checkQuantity, and now takes parameters for cleaner encapsulation.

--- src/main/java/me/wraeven/wraelibrary/gui/PagedMenu.java
+++ src/main/java/me/wraeven/wraelibrary/gui/PagedMenu.java
@@ -9,19 +9,22 @@ import java.util.Arrays;
 import java.util.List;
 
 public abstract class PagedMenu extends MenuHolder {
-    public PagedMenu() {
+    public PagedMenu(int maxItemsPerPage) {
         super();
+        if(checkQuantity(maxItemsPerPage)) {
+            this.maxItemsPerPage = maxItemsPerPage;
+        }
     }
 
     protected int page = 0;
     @Setter
     @Getter
-    protected int maxItemsPerPage = 45;
+    protected int maxItemsPerPage;
     protected int index = 0;
 
     private final List<Integer> acceptedMaxItems = Arrays.asList(9, 18, 27, 36, 45, 54);
 
-    public boolean check(int items) {
+    private boolean checkQuantity(int items) {
         return acceptedMaxItems.contains(items);
     }
 
